c6ecb821d658baeb90a691eace3246f21d84e987
Nikita karanpuria <nikita.karanpuria8@gmail.com>
2018-07-21 14:21:16 +0530

    frustrated

---
+++ Isogram/checkisogram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace IsogramProblem
+{
+    public class checkisogram
+    {
+        public static int IsIsogram( string str)
+        {
+            //str = str.ToLower();
+            int len = str.Length;
+            string iso = "";
+            foreach(char i in str)
+            { 
+                if (char.IsLetter(i))
+                {
+                    iso += i;
+                }
+            }
+            char[] arr = iso.ToCharArray();
+            Array.Sort(arr);
+            
+           // int flag=0;
+            for(int i = 0; i < iso.Length-1; i++)
+            {
+                
+                    if (arr[i] == arr[i + 1])
+                    {
+                        return 0;
+
+                    }
+                
+            }
+            return 1;
+        }
+    }
+}
